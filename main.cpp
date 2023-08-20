7fba685590dad3f69a74aeada25d0fe867535531
yashviradia <y.viradia@tu-braunschweig.de>
2023-08-20 13:38:30 +0200

    initial commit, completed topic lots of files

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "functions.h"
+
+using std::cout;
+using std::endl;
+
+
+int main() {
+    int x = 0;
+    x = increment(x);
+
+    cout << x << endl;
+    printMyName();
+
+    return 0;
+}
